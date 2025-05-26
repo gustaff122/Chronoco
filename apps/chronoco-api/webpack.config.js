fe12655779713fd196891410c2eb0726cd59a1ab
blazej.klopotek <bklopotek@9bits.pl>
2025-05-27 00:44:12 +0200

    Move ChronocoAPI app into apps directory

--- apps/chronoco-api/webpack.config.js
+++ apps/chronoco-api/webpack.config.js
@@ -3,7 +3,7 @@ const { join } = require('path');
 
 module.exports = {
   output: {
-    path: join(__dirname, '../dist/chronoco-api'),
+    path: join(__dirname, '../../dist/chronoco-api'),
   },
   plugins: [
     new NxAppWebpackPlugin({
