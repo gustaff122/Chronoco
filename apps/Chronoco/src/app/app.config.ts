94fd0652b008fbca255a22a77ff24b41c65d01d7
gustaff122 <blazej.klopotek@gmail.com>
2025-06-06 21:05:51 +0200

    update packages

--- apps/Chronoco/src/app/app.config.ts
+++ apps/Chronoco/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
@@ -6,7 +6,7 @@ import { provideClientHydration, withEventReplay } from '@angular/platform-brows
 export const appConfig: ApplicationConfig = {
   providers: [
     provideClientHydration(withEventReplay()),
-    provideExperimentalZonelessChangeDetection(),
+    provideZonelessChangeDetection(),
     provideRouter(appRoutes),
   ],
 };
--- libs/ui/components/scheduler/src/lib/scheduler-sidebar/components/scheduler-sidebar-blocks-list/scheduler-sidebar-blocks-list.component.ts
+++ libs/ui/components/scheduler/src/lib/scheduler-sidebar/components/scheduler-sidebar-blocks-list/scheduler-sidebar-blocks-list.component.ts
@@ -19,6 +19,6 @@ export class SchedulerSidebarBlocksListComponent {
   private readonly dialog: Dialog = inject(Dialog);
 
   public openAddModal(): void {
-
+    
   }
 }
