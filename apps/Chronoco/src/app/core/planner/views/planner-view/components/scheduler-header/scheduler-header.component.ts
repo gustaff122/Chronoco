0d29dee5a878c9656543f9782aecdc1f2656ff1e
kuczkoowska <jagoda.kucz@wp.pl>
2025-07-04 00:41:40 +0200

    [CHO-12] add row selector

--- apps/Chronoco/src/app/core/planner/views/planner-view/components/scheduler-header/scheduler-header.component.ts
+++ apps/Chronoco/src/app/core/planner/views/planner-view/components/scheduler-header/scheduler-header.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { provideIcons } from '@ng-icons/core';
 import { heroArrowDownTray, heroArrowUpTray, heroShare } from '@ng-icons/heroicons/outline';
 import { ButtonIconComponent } from '@chronoco-fe/ui/button-icon/button-icon.component';
+import { RowSelectorComponent } from '@chronoco-fe/ui/row-selector/row-selector.component';
 
 @Component({
   selector: 'app-scheduler-header',
@@ -9,6 +10,7 @@ import { ButtonIconComponent } from '@chronoco-fe/ui/button-icon/button-icon.com
   styleUrl: './scheduler-header.component.css',
   imports: [
     ButtonIconComponent,
+    RowSelectorComponent,
   ],
   viewProviders: [
     provideIcons({ heroArrowUpTray, heroArrowDownTray, heroShare }),
