31eb437dae649dcfef2a3956719442f4e1d26e60
gustaff122 <blazej.klopotek@gmail.com>
2025-07-04 21:04:33 +0200

    [CHO-14] add cursors on events

--- apps/Chronoco/src/app/core/planner/views/planner-view/components/scheduler-grid/components/scheduler-grid-blocks/components/scheduler-grid-single-block/scheduler-grid-single-block.component.ts
+++ apps/Chronoco/src/app/core/planner/views/planner-view/components/scheduler-grid/components/scheduler-grid-blocks/components/scheduler-grid-single-block/scheduler-grid-single-block.component.ts
@@ -1,9 +1,12 @@
 import { Component, input, InputSignal } from '@angular/core';
 import { IRenderableBlock } from '@chronoco-fe/models/i-event-block';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-scheduler-grid-single-block',
-  imports: [],
+  imports: [
+    DatePipe,
+  ],
   templateUrl: './scheduler-grid-single-block.component.html',
   styleUrl: './scheduler-grid-single-block.component.css',
 })
