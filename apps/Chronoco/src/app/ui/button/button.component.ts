f5858a94fde853e833d219c43887b1c735f6d66b
gustaff122 <blazej.klopotek@gmail.com>
2025-07-04 19:20:08 +0200

    [CHO-14] add severities

--- apps/Chronoco/src/app/ui/button/button.component.ts
+++ apps/Chronoco/src/app/ui/button/button.component.ts
@@ -1,14 +1,24 @@
-import { Component, input, InputSignal } from '@angular/core';
-import { NgClass } from '@angular/common';
+import { Component, computed, input, InputSignal, Signal } from '@angular/core';
+
+type BtnType = 'button' | 'submit' | 'reset'
+
+type BtnSeverity = 'primary' | 'outline'
 
 @Component({
   selector: 'app-button',
-  standalone: true,
   templateUrl: './button.component.html',
   styleUrls: [ './button.component.css' ],
-  imports: [ NgClass ],
 })
 export class ButtonComponent {
-  public extraClass: InputSignal<string> = input('');
-  public extraType: InputSignal<string> = input('button');
+  public btnType: InputSignal<BtnType> = input('button' as BtnType);
+  public severity: InputSignal<BtnSeverity> = input('primary' as BtnSeverity);
+
+  public readonly severityClasses: Signal<string> = computed(() => {
+    switch (this.severity()) {
+      case 'primary':
+        return 'bg-indigo-700 text-sm rounded-lg text-gray-200 py-2.5 px-5';
+      case 'outline':
+        return 'px-5 py-2 text-gray-300 hover:bg-zinc-700 bg-zinc-800 border-solid border-2 border-gray-500 rounded-lg text-sm';
+    }
+  });
 }
