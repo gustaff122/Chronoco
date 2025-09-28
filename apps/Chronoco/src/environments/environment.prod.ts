235626c9a2e0cdbba8adf8c59cd917d316185e30
gustaff122 <blazej.klopotek@gmail.com>
2025-09-28 20:45:55 +0200

    [CHO] fix socket's protocol

--- apps/Chronoco/src/environments/environment.prod.ts
+++ apps/Chronoco/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   API_URL: 'https://api-chronoco.dev122.pl/api',
-  SOCKET_PLANNER_URL: 'ws://api-chronoco.dev122.pl',
+  SOCKET_PLANNER_URL: 'wss://api-chronoco.dev122.pl',
 };
